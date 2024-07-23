57395168e41989ea347e1b5fc7fb952d37258225
Meltem <meltemak8@gmail.com>
2024-07-24 01:45:56 +0200

    Initial Commit

---
+++ TravelCheckList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelCheckList
+//
+//  Created by Meltem Uğurlu on 24.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelCheckList/TravelCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCheckListApp.swift
+//  TravelCheckList
+//
+//  Created by Meltem Uğurlu on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
